e0fdb5965fc393e4f35d7c088cce51a0d3e9ca95
Laurence2013 <djroa1985@gmail.com>
2018-06-07 22:22:02 +0100

    04 groopstr

---
+++ members/tests/behaviour_tests/pre_test.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class Pre_Test(TestCase):
+    def test_01_sample_test(self):
+        self.assertTrue(True)
+
+    def test_02_sample_test(self):
+        self.assertTrue(False)
