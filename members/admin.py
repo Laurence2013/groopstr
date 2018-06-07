f13fc6b749b645dd26933f5ad5871535a22d6e0e
Laurence2013 <djroa1985@gmail.com>
2018-06-07 21:52:02 +0100

    03 groopstr

--- members/admin.py
+++ members/admin.py
@@ -2,3 +2,16 @@ from django.contrib import admin
 from members.models import *
 
 admin.site.register(Player_table)
+admin.site.register(Form_table)
+admin.site.register(Clean_Sheets_table)
+admin.site.register(Goals_Assist_table)
+admin.site.register(Goals_table)
+admin.site.register(Man_of_Match_table)
+admin.site.register(Own_Goals_table)
+admin.site.register(Yellow_Car_table)
+admin.site.register(Red_Card_table)
+admin.site.register(Personal_Info_table)
+admin.site.register(Current_Position_table)
+admin.site.register(Members_table)
+admin.site.register(Team_table)
+admin.site.register(Player_Team_table)
