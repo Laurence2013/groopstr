b3b2582cc7340fa10e6515536b0156c4246cfe85
Laurence2013 <djroa1985@gmail.com>
2018-07-28 09:08:53 +0100

    09 groopstr

--- groopstr/urls.py
+++ groopstr/urls.py
@@ -20,4 +20,6 @@ from members.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='register'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import (
+    authenticate, get_user_model, login, logout
+)
 from django.views.generic import TemplateView
 from django.shortcuts import HttpResponse, render
 
@@ -7,4 +10,20 @@ class RegisterView(TemplateView):
         return context
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'index.html', self.get_context_data(**kwargs))
+        return render(request, 'register.html', self.get_context_data(**kwargs))
+
+class LoginView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(LoginView, self).get_context_data(**kwargs)
+        return context
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'login.html', self.get_context_data(**kwargs))
+
+class LogoutView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(LogoutView, self).get_context_data(**kwargs)
+        return context
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'logout.html', self.get_context_data(**kwargs))
