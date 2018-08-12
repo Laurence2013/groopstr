fe76dc64c1c1f570a060d96bc7ad9f63ee800f08
Laurence2013 <djroa1985@gmail.com>
2018-08-12 22:01:07 +0100

    28 groopstr

--- admin_updates/views.py
+++ admin_updates/views.py
@@ -8,17 +8,39 @@ from players.models import *
 class AdminUpdateView(View):
     def get(self, request, *args, **kwargs):
         # week = Week_table.objects.values('week_no').order_by('week_no').reverse()
-        week = [{'week_no': 1}, {'week_no': 1}, {'week_no': 2}, {'week_no': 2}, {'week_no': 3}, {'week_no': 3}, {'week_no': 4}, {'week_no': 4}]
+        # week = [{'week_no': 1}, {'week_no': 1}, {'week_no': 2}, {'week_no': 2}, {'week_no': 3}, {'week_no': 3}, {'week_no': 4}, {'week_no': 4}]
         # week = [{'week_no': 1}, {'week_no': 1}, {'week_no': 2}, {'week_no': 2}, {'week_no': 3}, {'week_no': 3}, {'week_no': 4}, {'week_no': 4}, {'week_no': 4}]
+        week = [{'week_no': 1}, {'week_no': 1}, {'week_no': 2}, {'week_no': 3}, {'week_no': 3}, {'week_no': 4}, {'week_no': 4}]
+        # week = [{'week_no': 1}, {'week_no': 2}, {'week_no': 2}, {'week_no': 3}, {'week_no': 3}, {'week_no': 4}, {'week_no': 4}]
         group_week = self.__group_ids(week)
-        group_week2 = self.__group_ids_2(group_week)
-        if group_week2 is False:
-            print(False)
-        else:
-            group_week3 = self.__group_ids_3(group_week)
+        # group_week = self.__group_ids_1(week)
+        # group_week2 = self.__group_ids_2(group_week)
+        # if group_week2 is False:
+        #     print(False)
+        # else:
+        #     group_week3 = self.__group_ids_3(group_week)
         return HttpResponse('Hello')
 
     def __group_ids(self, week):
+        week_right = []
+        week_left = []
+        is_end_week = 1
+        for i in range(0, len(week)):
+            if is_end_week < len(week):
+                if week[i] == week[i+1]:
+                    week_right.append(week[i].get('week_no'))
+                    week_left.append(week[i+1].get('week_no'))
+                is_end_week += 1
+        final_week = week_right + week_left
+        print(final_week)
+        is_end_week2 = 1
+        for j in range(0, len(week)):
+            if is_end_week2 < len(final_week):
+                if week[j].get('week_no') == final_week[is_end_week2]:
+                    print(week[j].get('week_no'), final_week[is_end_week2])
+            is_end_week2 += 1
+
+    def __group_ids_1(self, week):
         group_week = []
         is_end_week = 1
         for i in range(0, len(week)):
