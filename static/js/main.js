def1e603b9e028027aaa8f93530bdca9ddb6146c
Laurence2013 <djroa1985@gmail.com>
2018-08-14 23:01:34 +0100

    35 groopstr

--- static/js/main.js
+++ static/js/main.js
@@ -4,19 +4,20 @@ CreateANewRequest.prototype = {
   Weekly_Fixtures: function() {
     var http = new XMLHttpRequest();
     console.log(http);
-    // http.onreadystatechange = function() {
-    //   if(http.readyState == 4 && http.status == 200){
-    //       var cat_links = JSON.parse(http.responseText);
-    //       var mainHtml = '';
-    //       mainHtml = '<h2 class="category_nav">Popular Categories</h2>';
-    //       mainHtml += '<ul class="nav flex-column list-group">';
-    //       for (i = 0; i < cat_links.length; i++) {
-    //         mainHtml += '<li id="backg-colour" class="nav-item list-group-item"><a id="link-colour" class="nav-link active" href="'+ cat_links[i].pk +'">'+ cat_links[i]['fields'].cat_list + '</a></li>';
-    //       }
-    //       mainHtml += '</ul>';
-    //       category_links.innerHTML = mainHtml;
-    //   }
-    // }
+    http.onreadystatechange = function() {
+      if(http.readyState == 4 && http.status == 200){
+          var weekly_fixs = JSON.parse(http.responseText);
+          console.log(weekly_fixs);
+          var mainHtml = '';
+          mainHtml = '<h2 class="weekly_fixtures">Weekly Fixtures</h2>';
+          mainHtml += '<ul class="nav flex-column list-group">';
+          for (i = 0; i < weekly_fixs.length; i++) {
+            mainHtml += '<li id="backg-colour" class="nav-item list-group-item"><a id="link-colour" class="nav-link active" href="'+ weekly_fixs[i].id +'">'+ weekly_fixs[i].fixture + '</a></li>';
+          }
+          mainHtml += '</ul>';
+          weekly_fixtures.innerHTML = mainHtml;
+      }
+    }
     http.open("GET", "admin_get_fixtures", true);
     http.setRequestHeader('Content-type', 'application/json', true);
     http.send();
