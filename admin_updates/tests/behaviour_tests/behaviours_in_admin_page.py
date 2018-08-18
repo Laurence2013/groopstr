6d2504607f540d108b86f9bcf7c79a71f25b1709
Laurence2013 <djroa1985@gmail.com>
2018-08-18 16:18:06 +0100

    44 groopstr

--- admin_updates/tests/behaviour_tests/behaviours_in_admin_page.py
+++ admin_updates/tests/behaviour_tests/behaviours_in_admin_page.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from nose.tools import nottest
 from members.models import *
 from members.forms import *
+import datetime
 from test_database_tables import Fixtures_and_Weeks
 '''
 Scenario:-
@@ -16,11 +17,36 @@ Admin will always update players points into the players statistics tables
 class Behaviours_In_Admin_Page(TestCase):
     def setUp(self):
         self.test = Fixtures_and_Weeks()
+        self.week_fixture_0 = {'end_date': datetime.date(2018, 8, 11), 'fixture': 'Chelsea vs Fiorentina', 'date_of_game': datetime.date(2018, 8, 8), 'start_date': datetime.date(2018, 8, 5), 'week_no': 1, 'competition': 'cl'}
+        self.week_fixture_1 = {'end_date': datetime.date(2018, 8, 11), 'fixture': 'Chelsea vs Arsenal', 'date_of_game': datetime.date(2018, 8, 10), 'start_date': datetime.date(2018, 8, 5), 'week_no': 1, 'competition': 'pl'}
+        self.week_fixture_2 = {'end_date': datetime.date(2018, 8, 18), 'fixture': 'Liverpool vs Everton', 'date_of_game': datetime.date(2018, 8, 10), 'start_date': datetime.date(2018, 8, 12), 'week_no': 2, 'competition': 'pl'}
+        self.week_fixture_3 = {'fixture': 'Chelsea vs Fiorentina', 'week_no': 1, 'competition': 'cl', 'start_date': datetime.date(2018, 8, 5), 'date_of_game': datetime.date(2018, 8, 8), 'end_date': datetime.date(2018, 8, 11)}
+        self.week_fixture_4 = {'fixture': 'Chelsea vs Arsenal', 'week_no': 2, 'competition': 'pl', 'start_date': datetime.date(2018, 8, 12), 'date_of_game': datetime.date(2018, 8, 10), 'end_date': datetime.date(2018, 8, 18)}
+        self.week_fixture_5 = {'fixture': 'Liverpool vs Everton', 'week_no': 2, 'competition': 'pl', 'start_date': datetime.date(2018, 8, 12), 'date_of_game': datetime.date(2018, 8, 10), 'end_date': datetime.date(2018, 8, 18)}
+        self.week_fixture_6 = {'start_date': datetime.date(2018, 8, 12), 'end_date': datetime.date(2018, 8, 18), 'week_no': 2, 'date_of_game': datetime.date(2018, 8, 8), 'competition': 'cl', 'fixture': 'Chelsea vs Fiorentina'}
+        self.week_fixture_7 = {'start_date': datetime.date(2018, 8, 12), 'end_date': datetime.date(2018, 8, 18), 'week_no': 2, 'date_of_game': datetime.date(2018, 8, 10), 'competition': 'pl', 'fixture': 'Chelsea vs Arsenal'}
+        self.week_fixture_8 = {'start_date': datetime.date(2018, 8, 19), 'end_date': datetime.date(2018, 8, 26), 'week_no': 3, 'date_of_game': datetime.date(2018, 8, 10), 'competition': 'pl', 'fixture': 'Liverpool vs Everton'}
     '''
-    Show that a fixture Chelsea vs Fiorentina is connected to Week 1, its week_no_id is 11
-    Show that a fixture Chelsea vs Arsenal is connected to Week 1, its week_no_id is 11
+    Show a fixture, for example. Chelsea vs Fiorentina is connected to Week 1, its week_no_id is 11
+    Show a fixture, for example. Chelsea vs Arsenal is connected to Week 1, its week_no_id is 11
     '''
     def test_00_show_that_a_fixture_is_connected_to_a_week(self):
-        get_week = self.test.week_fixture_table()
-        self.test.set_fixtures_and_week(get_week)
-        
+        get_week = self.test.week_fixture_table_00()
+        get_week_fixture = self.test.set_fixtures_and_week(get_week)
+        self.assertDictEqual(self.week_fixture_0, get_week_fixture[0])
+        self.assertDictEqual(self.week_fixture_1, get_week_fixture[1])
+        self.assertDictEqual(self.week_fixture_2, get_week_fixture[2])
+
+    def test_01_show_that_a_fixture_is_connected_to_a_week(self):
+        get_week = self.test.week_fixture_table_01()
+        get_week_fixture = self.test.set_fixtures_and_week(get_week)
+        self.assertDictEqual(self.week_fixture_3, get_week_fixture[0])
+        self.assertDictEqual(self.week_fixture_4, get_week_fixture[1])
+        self.assertDictEqual(self.week_fixture_5, get_week_fixture[2])
+
+    def test_02_show_that_a_fixture_is_connected_to_a_week(self):
+        get_week = self.test.week_fixture_table_02()
+        get_week_fixture = self.test.set_fixtures_and_week(get_week)
+        self.assertDictEqual(self.week_fixture_6, get_week_fixture[0])
+        self.assertDictEqual(self.week_fixture_7, get_week_fixture[1])
+        self.assertDictEqual(self.week_fixture_8, get_week_fixture[2])
--- admin_updates/tests/behaviour_tests/test_database_tables.py
+++ admin_updates/tests/behaviour_tests/test_database_tables.py
@@ -4,7 +4,7 @@ from members.forms import *
 1 - Connect fixtures to its correct week before displaying - test_database_tables.Fixtures_and_Weeks class
 '''
 class Fixtures_and_Weeks:
-    def week_fixture_table(self):
+    def week_fixture_table_00(self):
         Week_table.objects.create(id = 11, week_no = 1, date_updated = '2018-08-17 20:44:40', end_date = '2018-08-11', start_date = '2018-08-05', is_current_week = 0)
         Week_table.objects.create(id = 12, week_no = 2, date_updated = '2018-08-17 20:44:46', end_date = '2018-08-18', start_date = '2018-08-12', is_current_week = 0)
         Week_table.objects.create(id = 13, week_no = 3, date_updated = '2018-08-17 20:44:50', end_date = '2018-08-26', start_date = '2018-08-19', is_current_week = 0)
@@ -12,6 +12,28 @@ class Fixtures_and_Weeks:
         Fixtures_table.objects.create(fixture = 'Chelsea vs Fiorentina', date_updated = '2018-08-12 16:47:37', competition = 'cl', date_of_game = '2018-08-08', week_no_id = 11)
         Fixtures_table.objects.create(fixture = 'Chelsea vs Arsenal', date_updated = '2018-08-12 16:47:37', competition = 'pl', date_of_game = '2018-08-10', week_no_id = 11)
         Fixtures_table.objects.create(fixture = 'Liverpool vs Everton', date_updated = '2018-08-12 16:47:37', competition = 'pl', date_of_game = '2018-08-10', week_no_id = 12)
+
+        return Week_table.objects.all().values('id','week_no','start_date','end_date','is_current_week'), Fixtures_table.objects.all().values('id','fixture','competition','date_of_game','week_no_id')
+
+    def week_fixture_table_01(self):
+        Week_table.objects.create(id = 11, week_no = 1, date_updated = '2018-08-17 20:44:40', end_date = '2018-08-11', start_date = '2018-08-05', is_current_week = 0)
+        Week_table.objects.create(id = 12, week_no = 2, date_updated = '2018-08-17 20:44:46', end_date = '2018-08-18', start_date = '2018-08-12', is_current_week = 0)
+        Week_table.objects.create(id = 13, week_no = 3, date_updated = '2018-08-17 20:44:50', end_date = '2018-08-26', start_date = '2018-08-19', is_current_week = 0)
+
+        Fixtures_table.objects.create(fixture = 'Chelsea vs Fiorentina', date_updated = '2018-08-12 16:47:37', competition = 'cl', date_of_game = '2018-08-08', week_no_id = 11)
+        Fixtures_table.objects.create(fixture = 'Chelsea vs Arsenal', date_updated = '2018-08-12 16:47:37', competition = 'pl', date_of_game = '2018-08-10', week_no_id = 12)
+        Fixtures_table.objects.create(fixture = 'Liverpool vs Everton', date_updated = '2018-08-12 16:47:37', competition = 'pl', date_of_game = '2018-08-10', week_no_id = 12)
+
+        return Week_table.objects.all().values('id','week_no','start_date','end_date','is_current_week'), Fixtures_table.objects.all().values('id','fixture','competition','date_of_game','week_no_id')
+
+    def week_fixture_table_02(self):
+        Week_table.objects.create(id = 11, week_no = 1, date_updated = '2018-08-17 20:44:40', end_date = '2018-08-11', start_date = '2018-08-05', is_current_week = 0)
+        Week_table.objects.create(id = 12, week_no = 2, date_updated = '2018-08-17 20:44:46', end_date = '2018-08-18', start_date = '2018-08-12', is_current_week = 0)
+        Week_table.objects.create(id = 13, week_no = 3, date_updated = '2018-08-17 20:44:50', end_date = '2018-08-26', start_date = '2018-08-19', is_current_week = 0)
+
+        Fixtures_table.objects.create(fixture = 'Chelsea vs Fiorentina', date_updated = '2018-08-12 16:47:37', competition = 'cl', date_of_game = '2018-08-08', week_no_id = 12)
+        Fixtures_table.objects.create(fixture = 'Chelsea vs Arsenal', date_updated = '2018-08-12 16:47:37', competition = 'pl', date_of_game = '2018-08-10', week_no_id = 12)
+        Fixtures_table.objects.create(fixture = 'Liverpool vs Everton', date_updated = '2018-08-12 16:47:37', competition = 'pl', date_of_game = '2018-08-10', week_no_id = 13)
 
         return Week_table.objects.all().values('id','week_no','start_date','end_date','is_current_week'), Fixtures_table.objects.all().values('id','fixture','competition','date_of_game','week_no_id')
 
@@ -24,12 +46,9 @@ class Fixtures_and_Weeks:
                         'fixture': get_week[1][j].get('fixture'),
                         'date_of_game': get_week[1][j].get('date_of_game'),
                         'competition': get_week[1][j].get('competition'),
-                        'week_no': get_week[0][j].get('week_no'),
-                        'start_date': get_week[0][j].get('start_date'),
-                        'end_date': get_week[0][j].get('end_date'),
+                        'week_no': get_week[0][i].get('week_no'),
+                        'start_date': get_week[0][i].get('start_date'),
+                        'end_date': get_week[0][i].get('end_date'),
                     }
                     week_fixture.append(context)
-        print()
-        print(week_fixture[0])
-        print(week_fixture[1])
-        print(week_fixture[2])
+        return week_fixture
