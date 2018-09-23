26200b9d8f270661f70c36ce9ecd75330365bc0b
Laurence2013 <djroa1985@gmail.com>
2018-09-23 23:49:22 +0100

    109 groopstr

--- static/js/statistics_table.js
+++ static/js/statistics_table.js
@@ -5,14 +5,31 @@
   http.onreadystatechange = function() {
     if (http.readyState == 4 && http.status == 200) {
       var get_all_stats = JSON.parse(http.responseText);
-      var get_name = get_all_stats.filter(function(stats){
+      console.log(get_all_stats);
+      var get_week = get_all_stats.filter(function(stats){
         if (stats[0].name === 'weeks') {
-          return stats[0].name;
+          return stats[0];
         }
       });
-      console.log(get_name);
+      var mainHtml = '';
+      mainHtml += '<h5><b>Check all </b>'+ get_week[0][0].name  +':</h5>';
+      mainHtml += '<select id="week_no" name="week_no">';
+      mainHtml += '<option>Select a week</option>';
+
+      let get_week_stats = function(week_len, week_no, sub_week_no) {
+        if (week_no === week_len) return;
+        mainHtml += '<option value="'+ get_week[0][week_no][sub_week_no].id +'">Week: '+ get_week[0][week_no][sub_week_no].week_no +'</option>';
+        get_week_stats(week_len, week_no + 1, sub_week_no + 1);
+      }
+      get_week_stats(get_week[0].length, week_no = 1, sub_week_no = 0);
+      mainHtml += '</select>';
+      get_all_stats_table.innerHTML = mainHtml;
+
+      document.getElementById('week_no').onclick = function EventHandler() {
+        console.log(this.value);
+      }
     }
   }
   http.send();
-  get_all_stats.innerHTML = 'All stats tables...';
+  get_all_stats_table.innerHTML = 'All stats tables...';
 })();
